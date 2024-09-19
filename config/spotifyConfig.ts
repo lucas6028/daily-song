63f37652924bd71fe3e02fa474a086fea256e374
hao <lucas60303@gmail.com>
2024-09-19 16:03:40 +0800

    Add spotifyConfig.ts

---
+++ config/spotifyConfig.ts
@@ -0,0 +1,9 @@
+import SpotifyWebAPI from "spotify-web-api-node";
+
+const spotifyAPI = new SpotifyWebAPI({
+  redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URL,
+  clientId: process.env.SECRET_CLIENT_ID,
+  clientSecret: process.env.SECRET_CLIENT_SECRET,
+});
+
+export default spotifyAPI;
