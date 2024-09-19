42f9d04882d60a6d2d876b35bf3dd0d7c44c0fc2
hao <lucas60303@gmail.com>
2024-09-19 10:52:03 +0800

    Add function redirectURL.ts

---
+++ src/utils/redirectURL.ts
@@ -0,0 +1,21 @@
+import { generateRandomString } from "./generateRandomString";
+
+export default function RedirectURL() {
+  const clientId = process.env.SECRET_CLIENT_ID || "";
+  const redirectURI = process.env.SECRET_CLIENT_SECRET || "";
+  const state = generateRandomString(128);
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", redirectURI);
+  params.append(
+    "scope",
+    "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read user-library-modify user-top-read"
+  );
+  params.append("state", state);
+  params.append("show_dialog", "true");
+
+  const spotifyAuthURL = `https://accounts.spotify.com/authorize?${params.toString()}`;
+  window.location.href = spotifyAuthURL;
+}
