f3adf23eb9a18efc5a17f59a06af888651371ba7
hao <lucas60303@gmail.com>
2024-09-19 13:45:24 +0800

    change export dashboard to Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function dashboard() {
+function Dashboard() {
     return (
         <h1>Dashboard</h1>
     )
 }
 
-export default dashboard
+export default Dashboard;
