f541c7a6fa63f4fcf3e0a55c39c1860c00eccd0c
hao <lucas60303@gmail.com>
2024-09-19 22:07:38 +0800

    Change the cliend_id in spotifyConfig.ts

--- config/spotifyConfig.ts
+++ config/spotifyConfig.ts
@@ -2,7 +2,7 @@ import SpotifyWebAPI from "spotify-web-api-node";
 
 const spotifyAPI = new SpotifyWebAPI({
   redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URL,
-  clientId: process.env.SECRET_CLIENT_ID,
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
   clientSecret: process.env.SECRET_CLIENT_SECRET,
 });
 
