7c9f9e280e6250fb3b4d67cdd15362ee3784e428
hao <lucas60303@gmail.com>
2024-09-20 19:48:00 +0800

    Add route /api/artist/related

---
+++ app/api/artist/related/route.ts
@@ -0,0 +1,27 @@
+import spotifyAPI from "config/spotifyConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const accessToken = request.cookies.get("access_token")?.value;
+    const searchParams = request.nextUrl.searchParams;
+
+    const id = searchParams.get("id") || "6HvZYsbFfjnjFrWF950C9d";
+    const market = searchParams.get("market") || "ES";
+
+    if (!accessToken) {
+      return NextResponse.json({
+        message: "Access token is missing",
+        status: 401,
+      });
+    }
+
+    spotifyAPI.setAccessToken(accessToken);
+    const data = await spotifyAPI.getArtistTopTracks(id, market);
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.error("Error while get related artist: " + err);
+    return NextResponse.json({ message: err, status: 401 });
+  }
+}
