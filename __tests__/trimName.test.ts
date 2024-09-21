8aab2719c3496c43e3dac41f17a32221c87cd4d0
hao <lucas60303@gmail.com>
2024-09-21 09:52:09 +0800

    Run trimName.test.ts sucessfully

---
+++ __tests__/trimName.test.ts
@@ -0,0 +1,26 @@
+import { test, expect, describe } from "vitest";
+import { trimName } from "lib/name";
+
+describe("trimName function", () => {
+  test("should trim the song name after hyphen (-)", () => {
+    expect(trimName("這麼多年 - 電影《我想和你在一起》插曲")).toBe("這麼多年");
+  });
+
+  test("should trim the song name after parentheses ( )", () => {
+    const cases = [
+      { input: "3D (feat. Jack Harlow)", output: "3D" },
+      {
+        input: "A Beautiful Life(From the Netflix Film 'A Beautiful Life')",
+        output: "A Beautiful Life",
+      },
+    ];
+
+    cases.forEach(({ input, output }) => {
+      expect(trimName(input)).toBe(output);
+    });
+  });
+
+  test("should not trim the name if no delimiter is present", () => {
+    expect(trimName("Dream")).toBe("Dream");
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      config: path.resolve(__dirname, "./config"),
+      components: path.resolve(__dirname, "./components"),
+      lib: path.resolve(__dirname, "./lib"),
+      styles: path.resolve(__dirname, "./styles"),
+      types: path.resolve(__dirname, "./types"),
+    },
   },
 });
