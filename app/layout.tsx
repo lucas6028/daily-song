3ca2e4a1f07ce8722bcf24c682e1702d75ccfef0
hao <lucas60303@gmail.com>
2024-09-21 10:37:01 +0800

    Add open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Daily Song" />
+        <meta
+          property="og:description"
+          content="Display top tracks, recommend tracks, and daily challenge"
+        />
+        <meta property="og:image" content="/large_icon.png" />
+        <meta property="og:url" content="https://dailysong.vercel.app" />
+        <meta property="og:type" content="website" />
       </head>
       <body>{children}</body>
     </html>
