99dcadecd236ca48336fb16c822544a8e24ee080
hao <lucas60303@gmail.com>
2024-09-26 10:17:47 +0800

    Modify next.config.mjs to add HTTP header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,53 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/(.*)',
+        headers: [
+          // a security feature that allows you to control which sites can access your resources.
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none';",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload', // Two years HSTS policy
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff', // Prevent MIME type sniffing
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY', // Prevent clickjacking
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer', // No referrer information will be sent
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self), microphone=()', // Only allow geolocation and microphone permissions as needed
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
