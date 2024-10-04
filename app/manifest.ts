bda8bd38d48986387dba2fe33ca8c6752f6ec2d0
hao <lucas60303@gmail.com>
2024-10-04 21:26:38 +0800

    Add manifest.ts to add PWA to website

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'DailySong PWA',
+    short_name: 'DailySongPWA',
+    description: 'Display top tracks, recommend tracks, and daily challenge',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: '192x192',
+        type: 'image/svg',
+      },
+      {
+        src: '/images/small_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+}
