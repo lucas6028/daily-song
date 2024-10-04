d134abde3767cb48a284d3903322bc90fc47aab7
hao <lucas60303@gmail.com>
2024-10-04 21:29:23 +0800

    Change the icon of PWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: '192x192',
-        type: 'image/svg',
-      },
-      {
         src: '/images/small_icon.png',
         sizes: '512x512',
         type: 'image/png',
+      },
+      {
+        src: '/favicon.ico',
+        sizes: '192x192',
+        type: 'image/svg',
       },
     ],
   };
