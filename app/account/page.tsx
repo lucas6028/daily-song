59bbf5ac9a9d63bdf8c7c542751942dcf974ad7f
hao <lucas60303@gmail.com>
2024-10-07 16:43:55 +0800

    Format prettier

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+'use client';
 
-import Footer from 'components/Layout/Footer'
-import NavBar from 'components/Layout/Navbar'
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import Footer from 'components/Layout/Footer';
+import NavBar from 'components/Layout/Navbar';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function Account() {
-    return (
-        <Container className="mx-auto p-2">
-            <NavBar />
-            <h1>Account</h1>
-            <Footer />
-        </Container>
-    )
+  return (
+    <Container className="mx-auto p-2">
+      <NavBar />
+      <h1>Account</h1>
+      <Footer />
+    </Container>
+  );
 }
 
-export default Account
+export default Account;
--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+'use client';
 
-import Footer from 'components/Layout/Footer'
-import NavBar from 'components/Layout/Navbar'
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import Footer from 'components/Layout/Footer';
+import NavBar from 'components/Layout/Navbar';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function Setting() {
-    return (
-        <Container className="mx-auto p-2">
-            <NavBar />
-            <h1>Setting</h1>
-            <Footer />
-        </Container>
-    )
+  return (
+    <Container className="mx-auto p-2">
+      <NavBar />
+      <h1>Setting</h1>
+      <Footer />
+    </Container>
+  );
 }
 
-export default Setting
+export default Setting;
