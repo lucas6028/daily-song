3b7adb4a3bdf04cc2442427dc09c000c3d5b06c1
hao <lucas60303@gmail.com>
2024-10-10 12:06:09 +0800

    Delete /test-error

--- app/test-error/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function TestErrorPage() {
-  throw new Error('This is a test error');
-
-  // This code will never be reached
-  return <div>This page intentionally throws an error</div>;
-}
