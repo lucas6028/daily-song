4400a5fce28c46adc5cc54a62099c9486840b695
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-02 09:02:15 +0000

    Display tracks that Gemini recommends

--- app/recommend/page.tsx
+++ app/recommend/page.tsx
@@ -1,23 +1,137 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
-import { Container } from 'react-bootstrap';
+import { Carousel, Card, Container, Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import PlayButton from 'components/UI/PlayButton';
 import NavBar from 'components/Layout/Navbar';
+import axios from 'axios';
+import styles from 'styles/DailySong.module.css';
+import spotifyPlayerStyles from 'styles/spotifyPlayerStyle';
 import Footer from 'components/Layout/Footer';
-import FixingPage from 'components/Layout/FixingPage';
+import Loading from './loading';
+import { useRecommendedTracks } from 'hooks/useRecommendTracks';
+import { sleep } from 'lib/sleep';
+import { useTopArtist } from 'hooks/useTopArtist';
+
+// Dynamically import SpotifyWebPlayer
+const SpotifyWebPlayer = dynamic(() => import('react-spotify-web-playback'), {
+  ssr: false, // Disable server-side rendering for this component
+  loading: () => <div>Loading player...</div>,
+});
 
 function Recommend() {
-  const errorMessages = [
-    'Service is temporarily unavailable.',
-    'The API endpoint is deprecated by Spotify.',
-    'Please try again in a few minutes.',
-  ];
+  const minPopularity = 10;
+  const LIMIT = 1;
+  const OFFSET = Math.floor(Math.random() * 21);
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const [uri, setUri] = useState<string>('');
+  const [play, setPlay] = useState<boolean>(false);
+  const [access_token, setAccessToken] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  // SWR
+  const fetcher = (url: string) => axios.get(url, { withCredentials: true }).then(res => res.data);
+  const { data: authData, error: authError } = useSWR('/api/auth', fetcher, {
+    revalidateOnFocus: false,
+  });
+  const { data: tokenData, error: tokenError } = useSWR('/api/auth/token', fetcher, {
+    revalidateOnFocus: false,
+  });
+  const { artists, artistsError } = useTopArtist(LIMIT, OFFSET);
+  const { tracks, isLoading, tracksError } = useRecommendedTracks(artists, minPopularity);
+
+  useEffect(() => {
+    if (authError) {
+      console.error('Error checking auth status:', authError);
+      router.push('/login');
+    } else if (authData && !authData.authenticated) {
+      router.push('/login');
+    } else if (authData && authData.authenticated) {
+      setIsAuthenticated(true);
+    }
+  }, [authData, authError]);
+
+  useEffect(() => {
+    if (tokenError) {
+      console.error('Error while getting token:', tokenError);
+    } else if (tokenData) {
+      setAccessToken(tokenData.access_token.value);
+    }
+  }, [tokenData, tokenError]);
+
+  useEffect(() => {
+    setPlay(true);
+  }, [uri]);
+
+  useEffect(() => {
+    if (!isLoading && !artistsError && isAuthenticated && !tracksError) {
+      setIsReady(true);
+      sleep(1000);
+    }
+  }, [isLoading, artistsError, isAuthenticated, tracksError]);
+
+  if (!isReady) {
+    return <Loading />;
+  }
+  if (artistsError) {
+    throw new Error(artistsError);
+  }
+  if (tracksError) {
+    throw new Error(tracksError);
+  }
   return (
     <>
       <Container className="my-1">
         <NavBar />
-        <FixingPage messages={errorMessages} />
+        <Carousel>
+          {tracks.map(track => (
+            <Carousel.Item key={track.track_uri}>
+              <Row className="justify-content-center">
+                <Col xs={12} md={6} lg={4}>
+                  <Card className={`bg-gradient text-dark ${styles.card}`}>
+                    <Card.Img
+                      variant="top"
+                      src={track.img_url}
+                      width={300}
+                      height={300}
+                      style={{ objectFit: 'contain' }}
+                    />
+                    <Card.Body className="d-flex flex-column align-items-center">
+                      <Card.Title className={styles.text}>{track.title}</Card.Title>
+                      <Card.Text className={styles.text}>{track.artist_name}</Card.Text>
+                      <PlayButton onClick={() => setUri(track.track_uri)} />
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </Container>
+      <div className={styles.playerContainer}>
+        {access_token ? (
+          <SpotifyWebPlayer
+            callback={state => {
+              if (!state.isPlaying) {
+                setPlay(false);
+              }
+            }}
+            showSaveIcon
+            play={play}
+            token={access_token}
+            uris={[uri]}
+            initialVolume={50}
+            styles={spotifyPlayerStyles}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
       <Footer />
     </>
   );
