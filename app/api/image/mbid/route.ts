7f090f44976a3a1f0b1665b2e05622b48509151a
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-07 03:43:40 +0000

    format prettier

--- app/api/image/mbid/route.ts
+++ app/api/image/mbid/route.ts
@@ -6,27 +6,27 @@ export async function GET(request: NextRequest) {
   const mbid = searchParams.get('mbid') || '650e7db6-b795-4eb5-a702-5ea2fc46c848';
 
   const url = 'https://musicbrainz.org/ws/2/artist/' + mbid + '?inc=url-rels&fmt=json';
-// const url = 'https://musicbrainz.org/ws/2/artist/381086ea-f511-4aba-bdf9-71c753dc5077?inc=url-rels&fmt=json';
+  // const url = 'https://musicbrainz.org/ws/2/artist/381086ea-f511-4aba-bdf9-71c753dc5077?inc=url-rels&fmt=json';
 
   try {
     const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'daily-song',
-        },
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'daily-song',
+      },
     });
     const data = await response.json();
     const relations = data.relations;
     for (let i = 0; i < relations.length; i++) {
-        if (relations[i].type === 'image') {
-            let image_url = relations[i].url.resource;
-            if (image_url.startsWith('https://commons.wikimedia.org/wiki/File:')) {
-                const filename = image_url.substring(image_url.lastIndexOf('/') + 1);
-                image_url = 'https://commons.wikimedia.org/wiki/Special:Redirect/file/' + filename;
-            }
-            // console.log(image_url);
+      if (relations[i].type === 'image') {
+        let image_url = relations[i].url.resource;
+        if (image_url.startsWith('https://commons.wikimedia.org/wiki/File:')) {
+          const filename = image_url.substring(image_url.lastIndexOf('/') + 1);
+          image_url = 'https://commons.wikimedia.org/wiki/Special:Redirect/file/' + filename;
         }
+        // console.log(image_url);
+      }
     }
 
     return NextResponse.json({ data: data.relations, status: 200 });
