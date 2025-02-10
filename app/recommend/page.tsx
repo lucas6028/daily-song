36713d28dfb2a34a4ee48b878dcadd755451802d
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-10 13:31:58 +0000

    optimize recommend tracks page

--- app/recommend/page.tsx
+++ app/recommend/page.tsx
@@ -1,57 +1,45 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
 import { Carousel, Card, Container, Row, Col } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 import { useRouter } from 'next/navigation';
 import PlayButton from 'components/UI/PlayButton';
 import NavBar from 'components/Layout/Navbar';
 import axios from 'axios';
-import styles from 'styles/DailySong.module.css';
 import Footer from 'components/Layout/Footer';
 import Loading from './loading';
+import styles from 'styles/DailySong.module.css';
 import { useRecommendedTracks } from 'hooks/useRecommendTracks';
-import { sleep } from 'lib/sleep';
 
 function Recommend() {
   const LIMIT = 10;
   const OFFSET = Math.floor(Math.random() * 21);
-  const [isReady, setIsReady] = useState<boolean>(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const router = useRouter();
 
-  // SWR
+  // Fetch auth status
   const fetcher = (url: string) => axios.get(url, { withCredentials: true }).then(res => res.data);
   const { data: authData, error: authError } = useSWR('/api/auth', fetcher, {
     revalidateOnFocus: false,
   });
+
+  // Redirect to login if not authenticated
+  useEffect(() => {
+    if (authError || (authData && !authData.authenticated)) {
+      router.push('/login');
+    }
+  }, [authData, authError, router]);
+
+  // Only request recommended tracks if authenticated
   const { tracks, isLoading, tracksError } = useRecommendedTracks(
-    'Paz8PyxaWKr82o2PlV',
+    authData && authData.authenticated ? 'Paz8PyxaWKr82o2PlV' : '',
     'TW',
     LIMIT,
     OFFSET
   );
 
-  useEffect(() => {
-    if (authError) {
-      console.error('Error checking auth status:', authError);
-      router.push('/login');
-    } else if (authData && !authData.authenticated) {
-      router.push('/login');
-    } else if (authData && authData.authenticated) {
-      setIsAuthenticated(true);
-    }
-  }, [authData, authError]);
-
-  useEffect(() => {
-    if (!isLoading && isAuthenticated && !tracksError) {
-      setIsReady(true);
-      sleep(1000);
-    }
-  }, [isLoading, isAuthenticated, tracksError]);
-
-  if (!isReady) {
+  // Show loading until auth and track data are ready
+  if (!authData || isLoading || !authData.authenticated) {
     return <Loading />;
   }
   if (tracksError) {
@@ -59,36 +47,34 @@ function Recommend() {
   }
 
   return (
-    <>
-      <Container className="my-1">
-        <NavBar />
-        <Carousel>
-          {tracks.map(track => (
-            <Carousel.Item key={track.title}>
-              <Row className="justify-content-center">
-                <Col xs={12} md={6} lg={4}>
-                  <Card className={`bg-gradient text-dark ${styles.card}`}>
-                    <Card.Img
-                      variant="top"
-                      src={track.img}
-                      width={300}
-                      height={300}
-                      style={{ objectFit: 'contain' }}
-                    />
-                    <Card.Body className="d-flex flex-column align-items-center">
-                      <Card.Title className={styles.text}>{track.title}</Card.Title>
-                      <Card.Text className={styles.text}>{track.artist}</Card.Text>
-                      <PlayButton onClick={() => (window.location.href = `${track.trackUri}`)} />
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </Container>
+    <Container className="my-1">
+      <NavBar />
+      <Carousel>
+        {tracks.map(track => (
+          <Carousel.Item key={track.title}>
+            <Row className="justify-content-center">
+              <Col xs={12} md={6} lg={4}>
+                <Card className={`bg-gradient text-dark ${styles.card}`}>
+                  <Card.Img
+                    variant="top"
+                    src={track.img}
+                    width={300}
+                    height={300}
+                    style={{ objectFit: 'contain' }}
+                  />
+                  <Card.Body className="d-flex flex-column align-items-center">
+                    <Card.Title className={styles.text}>{track.title}</Card.Title>
+                    <Card.Text className={styles.text}>{track.artist}</Card.Text>
+                    <PlayButton onClick={() => (window.location.href = `${track.trackUri}`)} />
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Carousel.Item>
+        ))}
+      </Carousel>
       <Footer />
-    </>
+    </Container>
   );
 }
 
