21db3e902a88a27b9096bab5235974d84e28bcff
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-11 14:22:28 +0000

    add API endpoint for related artists from LastFM API

--- app/api/artist/related/route.ts
+++ app/api/artist/related/route.ts
@@ -1,24 +1,17 @@
-import spotifyAPI from 'config/spotifyConfig';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   try {
-    const accessToken = request.cookies.get('access_token')?.value;
     const searchParams = request.nextUrl.searchParams;
+    const seed_artist = searchParams.get('seed_artist') || '周杰倫';
+    const limit = searchParams.get('limit') || 10;
+    const apiKey = process.env.SECRET_LASTFM_API_KEY;
 
-    const id = searchParams.get('id') || '6HvZYsbFfjnjFrWF950C9d';
+    const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${seed_artist}&limit=${limit}&api_key=${apiKey}&format=json`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-    if (!accessToken) {
-      return NextResponse.json({
-        message: 'Access token is missing',
-        status: 401,
-      });
-    }
-
-    spotifyAPI.setAccessToken(accessToken);
-    const data = await spotifyAPI.getArtistRelatedArtists(id);
-
-    return NextResponse.json(data);
+    return NextResponse.json({ artists: data.similarartists.artist, status: 200 });
   } catch (err) {
     console.error('Error while get related artist: ' + err);
     return NextResponse.json({ message: err, status: 401 });
