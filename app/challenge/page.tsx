411899cfa2c5d8735ca5ed92a3c43bbe71cee946
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-11 14:22:49 +0000

    modified related artist in ch

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -9,12 +9,19 @@ import axios from 'axios';
 import SpotifyWebPlayer from 'react-spotify-web-playback';
 import spotifyPlayerStyles from 'styles/spotifyPlayerStyle';
 import styles from 'styles/Challenge.module.css';
-import { Artist, SpotifyArtistResponse, SpotifyTracksResponse, Track } from 'types/types';
+import {
+  Artist,
+  LastfmArtist,
+  SpotifyArtistResponse,
+  SpotifyTracksResponse,
+  Track,
+} from 'types/types';
 import NavBar from 'components/Layout/Navbar';
 import { compareNames, trimName } from 'lib/name';
 import Footer from 'components/Layout/Footer';
 import Loading from './loading';
 import shuffleArray from 'lib/suffleArray';
+import FixingPage from 'components/Layout/FixingPage';
 
 function Challenge() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
@@ -27,7 +34,7 @@ function Challenge() {
   const [tracks, setTracks] = useState<Track[]>([]);
   const [selectedTrack, setSelectedTrack] = useState<string>('');
   const [selectedArtists, setSelectedArtists] = useState<string>('');
-  // const [relatedArtists, setRelatedArtists] = useState<Artist[]>([]);
+  const [relatedArtists, setRelatedArtists] = useState<Artist[]>([]);
   const [isFlipped, setIsFlipped] = useState(false);
   const router = useRouter();
 
@@ -67,7 +74,7 @@ function Challenge() {
       try {
         const res = await axios.get('/api/artist/my-top', {
           params: {
-            limit: 4,
+            limit: 1,
             offset: Math.floor(Math.random() * 51),
           },
           withCredentials: true,
@@ -75,11 +82,9 @@ function Challenge() {
         const newArtists: Artist[] = res.data.body.items.map((art: SpotifyArtistResponse) => ({
           name: art.name,
           id: art.id,
-          popularity: art.popularity,
           uri: art.uri,
-          imgUrl: art.images[1].url,
         }));
-        setArtists(shuffleArray(newArtists));
+        setArtists(newArtists);
       } catch (err) {
         console.error('Error while fetching top artists: ' + err);
         setError('Failed to fetch top artists');
@@ -89,47 +94,54 @@ function Challenge() {
     fetchTopArtists();
   }, [access_token]);
 
-  // useEffect(() => {
-  //   const fetchRelatedArtists = async () => {
-  //     if (artists.length === 0) return;
+  useEffect(() => {
+    const fetchRelatedArtists = async () => {
+      if (artists.length === 0) return;
 
-  //     try {
-  //       const res = await axios.get('/api/artist/related', {
-  //         params: {
-  //           id: artists[0].id,
-  //         },
-  //         withCredentials: true,
-  //       });
-  //       const newRelatedArtists: Artist[] = res.data.body.artists.map(
-  //         (art: SpotifyArtistResponse) => ({
-  //           name: art.name,
-  //           id: art.id,
-  //           popularity: art.popularity,
-  //           uri: art.uri,
-  //           imgUrl: art.images[1].url,
-  //         })
-  //       );
-  //       setRelatedArtists(shuffleArray(newRelatedArtists));
-  //     } catch (err) {
-  //       console.error('Error while get related artists: ' + err);
-  //     }
-  //   };
+      try {
+        const res = await axios.get('/api/artist/related', {
+          params: {
+            seed_artist: artists[0].name,
+            limit: 3,
+          },
+          withCredentials: true,
+        });
+        const newRelatedArtists: Artist[] = res.data.artists.map((art: LastfmArtist) => ({
+          name: art.name,
+          id: art.mbid,
+          uri: art.url,
+        }));
+        console.log('artists: ' + artists.map(art => art.name));
+        console.log('new related artists: ' + newRelatedArtists.map(art => art.name));
+        setRelatedArtists(shuffleArray([...artists, ...newRelatedArtists]));
+        console.log('related artists: ' + relatedArtists.map(art => art.name));
+      } catch (err) {
+        console.error('Error while get related artists: ' + err);
+      }
+    };
 
-  //   fetchRelatedArtists();
-  // }, [artists]);
+    fetchRelatedArtists();
+  }, [artists]);
 
   useEffect(() => {
     const fetchArtistTopTracks = async () => {
-      if (artists.length === 0) return;
-      const randomIndex = Math.floor(Math.random() * 4);
+      if (relatedArtists.length === 0) return;
 
-      // console.log('randomIndex:' + randomIndex);
-      // console.log('answer artist: ' + relatedArtists[randomIndex].name);
+      let answerIndex = 0;
+      for (let i = 0; i < relatedArtists.length; i++) {
+        if (relatedArtists[i].name === artists[0].name) {
+          answerIndex = i;
+          break;
+        }
+      }
+
+      console.log('answerIndex:' + answerIndex);
+      console.log('answer artist: ' + relatedArtists[answerIndex].name);
 
       try {
         const res = await axios.get<SpotifyTracksResponse>('/api/artist/top-tracks', {
           params: {
-            id: artists[randomIndex].id,
+            id: relatedArtists[answerIndex].id,
             market: 'TW',
           },
           withCredentials: true,
@@ -154,7 +166,7 @@ function Challenge() {
     };
 
     fetchArtistTopTracks();
-  }, [artists]);
+  }, [relatedArtists]);
 
   useEffect(() => {
     if (tracks.length === 0) return;
@@ -191,7 +203,7 @@ function Challenge() {
   if (
     !isAuthenticated ||
     artists.length === 0 ||
-    // relatedArtists.length === 0 ||
+    relatedArtists.length === 0 ||
     tracks.length === 0
   ) {
     return <Loading />;
@@ -201,123 +213,128 @@ function Challenge() {
     throw new Error(error);
   }
 
+  if (relatedArtists.length < 4) {
+    return <FixingPage messages={['There are some errors.', 'Please reload the page!']} />;
+  }
+
   return (
-    <Container className="mx-auto p-2">
-      <NavBar />
-      <div className={`${styles.cardContainer} ${isFlipped ? styles.flipped : ''}`}>
-        <div className={`d-flex justify-content-center mt-4 ${styles.cardInner}`}>
-          {/* Front Side */}
-          <div className={`d-flex justify-content-center mt-4 ${styles.Front}`}>
-            <Card className={`shadow-lg text-light rounded-3 profile-card ${styles.card}`}>
-              <Card.Body className="p-4">
-                <Form onSubmit={handleSubmit}>
-                  <h5 className="text-center mb-4">Music Selection</h5>
+    <>
+      <Container className="mx-auto p-2">
+        <NavBar />
+        <div className={`${styles.cardContainer} ${isFlipped ? styles.flipped : ''}`}>
+          <div className={`d-flex justify-content-center mt-4 ${styles.cardInner}`}>
+            {/* Front Side */}
+            <div className={`d-flex justify-content-center mt-4 ${styles.Front}`}>
+              <Card className={`shadow-lg text-light rounded-3 profile-card ${styles.card}`}>
+                <Card.Body className="p-4">
+                  <Form onSubmit={handleSubmit}>
+                    <h5 className="text-center mb-4">Music Selection</h5>
 
-                  {/* Song Input */}
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Song</Form.Label>
-                    <Form.Control
-                      placeholder="Enter song name..."
-                      onChange={handleTrackChange}
-                      value={selectedTrack}
-                      className="border-dark"
-                    />
-                  </Form.Group>
+                    {/* Song Input */}
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Label>Song</Form.Label>
+                      <Form.Control
+                        placeholder="Enter song name..."
+                        onChange={handleTrackChange}
+                        value={selectedTrack}
+                        className="border-dark"
+                      />
+                    </Form.Group>
 
-                  {/* Singer Select */}
-                  <Form.Group className="mb-3" controlId="formSelect">
-                    <Form.Label>Singer</Form.Label>
-                    <Form.Select
-                      value={selectedArtists}
-                      onChange={handleSingerChange}
-                      className="border-dark"
-                    >
-                      <option value="" disabled hidden>
-                        Select an artist...
-                      </option>
-                      <option value={artists[0].name}>{artists[0].name}</option>
-                      <option value={artists[1].name}>{artists[1].name}</option>
-                      <option value={artists[2].name}>{artists[2].name}</option>
-                      <option value={artists[3].name}>{artists[3].name}</option>
-                    </Form.Select>
-                  </Form.Group>
+                    {/* Singer Select */}
+                    <Form.Group className="mb-3" controlId="formSelect">
+                      <Form.Label>Singer</Form.Label>
+                      <Form.Select
+                        value={selectedArtists}
+                        onChange={handleSingerChange}
+                        className="border-dark"
+                      >
+                        <option value="" disabled hidden>
+                          Select an artist...
+                        </option>
+                        <option value={relatedArtists[0].name}>{relatedArtists[0].name}</option>
+                        <option value={relatedArtists[1].name}>{relatedArtists[1].name}</option>
+                        <option value={relatedArtists[2].name}>{relatedArtists[2].name}</option>
+                        <option value={relatedArtists[3].name}>{relatedArtists[3].name}</option>
+                      </Form.Select>
+                    </Form.Group>
 
-                  {/* Button Group */}
-                  <div className="d-flex justify-content-between mt-4">
-                    <Button
-                      variant="info"
-                      className={`w-100 me-2 ${styles.frontButtonColor}`}
-                      type="submit"
-                    >
-                      Submit
-                    </Button>
-                    <Button
-                      variant="info"
-                      className={`w-100 me-2 ${styles.frontButtonColor}`}
-                      onClick={() => setPlay(!play)}
-                    >
-                      Play
-                    </Button>
-                    <Button
-                      variant="info"
-                      className={`w-100 me-2 ${styles.frontButtonColor}`}
-                      onClick={() => setIsFlipped(!isFlipped)}
-                    >
-                      Answer
-                    </Button>
-                  </div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </div>
+                    {/* Button Group */}
+                    <div className="d-flex justify-content-between mt-4">
+                      <Button
+                        variant="info"
+                        className={`w-100 me-2 ${styles.frontButtonColor}`}
+                        type="submit"
+                      >
+                        Submit
+                      </Button>
+                      <Button
+                        variant="info"
+                        className={`w-100 me-2 ${styles.frontButtonColor}`}
+                        onClick={() => setPlay(!play)}
+                      >
+                        Play
+                      </Button>
+                      <Button
+                        variant="info"
+                        className={`w-100 me-2 ${styles.frontButtonColor}`}
+                        onClick={() => setIsFlipped(!isFlipped)}
+                      >
+                        Answer
+                      </Button>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
 
-          {/* Back Side */}
-          <div className={`d-flex justify-content-center mt-4 ${styles.cardBack}`}>
-            <Card className={`shadow-lg text-light rounded-3 profile-card ${styles.card}`}>
-              <Card.Img
-                src={tracks[0].img}
-                alt="Track cover"
-                className="rounded-circle mx-auto mt-3 profile-img"
-                style={{ width: '150px', height: '150px', objectFit: 'cover' }}
-              />
-              <Card.Body className="text-center p-4">
-                <Card.Title>{tracks[0].title}</Card.Title>
-                <Card.Subtitle className="mb-3">{tracks[0].artist}</Card.Subtitle>
-                <Button
-                  variant="info"
-                  className={`w-100 ${styles.frontButtonColor}`}
-                  onClick={() => setIsVisible(!isVisible)}
-                >
-                  Show Player
-                </Button>
-              </Card.Body>
-            </Card>
+            {/* Back Side */}
+            <div className={`d-flex justify-content-center mt-4 ${styles.cardBack}`}>
+              <Card className={`shadow-lg text-light rounded-3 profile-card ${styles.card}`}>
+                <Card.Img
+                  src={tracks[0].img}
+                  alt="Track cover"
+                  className="rounded-circle mx-auto mt-3 profile-img"
+                  style={{ width: '150px', height: '150px', objectFit: 'cover' }}
+                />
+                <Card.Body className="text-center p-4">
+                  <Card.Title>{tracks[0].title}</Card.Title>
+                  <Card.Subtitle className="mb-3">{tracks[0].artist}</Card.Subtitle>
+                  <Button
+                    variant="info"
+                    className={`w-100 ${styles.frontButtonColor}`}
+                    onClick={() => setIsVisible(!isVisible)}
+                  >
+                    Show Player
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Spotify Player */}
-      <div
-        className={`${isVisible ? styles.visible : styles.hidden} ${styles.playerContainer} mt-3`}
-      >
-        {access_token ? (
-          <SpotifyWebPlayer
-            callback={state => {
-              if (!state.isPlaying) setPlay(false);
-            }}
-            showSaveIcon
-            play={play}
-            token={access_token}
-            uris={[uri]}
-            initialVolume={50}
-            styles={spotifyPlayerStyles}
-          />
-        ) : (
-          <></>
-        )}
-      </div>
-
+        {/* Spotify Player */}
+        <div
+          className={`${isVisible ? styles.visible : styles.hidden} ${styles.playerContainer} mt-3`}
+        >
+          {access_token ? (
+            <SpotifyWebPlayer
+              callback={state => {
+                if (!state.isPlaying) setPlay(false);
+              }}
+              showSaveIcon
+              play={play}
+              token={access_token}
+              uris={[uri]}
+              initialVolume={50}
+              styles={spotifyPlayerStyles}
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+      </Container>
       <Footer />
-    </Container>
+    </>
   );
 }
 
